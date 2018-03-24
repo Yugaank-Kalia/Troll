82d6f072cb0e3a434ed19a56c1bcb6c51ab8737d
Yugaank Kalia <yugaank@outlook.com>
2018-03-24 10:26:46 +0530

    Add files via upload

---
+++ lol.py
@@ -0,0 +1,15 @@
+import numpy as np
+from PIL import ImageGrab
+import cv2
+import time
+
+last_time = time.time()
+while(True):
+    screen = np.array(ImageGrab.grab(bbox=(0, 40, 800, 640)))
+    print("The loop took {} seconds".format(time.time()-last_time))
+    last_time = time.time()
+    cv2.imshow('LEL', cv2.cvtColor(screen, cv2.COLOR_BGR2RGB))
+    if(cv2.waitkey(25) & 0xFF == ord('q')):
+        cv2.destroyAllWindows()
+        break
+
